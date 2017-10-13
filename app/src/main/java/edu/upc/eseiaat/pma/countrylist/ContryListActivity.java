48c0e12b825a00c36a3bb271b47ae55946ed0f70
AlejandroJG <ajaimezg@hotmail.com>
2017-10-13 16:20:50 +0200

    PrimerCommit

---
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/ContryListActivity.java
@@ -0,0 +1,38 @@
+package edu.upc.eseiaat.pma.countrylist;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ContryListActivity extends AppCompatActivity {
+
+    private ArrayList<String> country_list;
+    private ArrayAdapter<String> adaptador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contry_list);
+
+        String[] countries = getResources().getStringArray(R.array.countries);
+        country_list = new ArrayList<>(Arrays.asList(countries));
+        ListView list = (ListView) findViewById(R.id.CountryList);
+
+        adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, country_list );
+        list.setAdapter(adaptador);
+
+
+
+
+
+    }
+}
